29fd0419b44a18302e9890c1c1d156f269a757e9
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-04 18:22:24 +0700

    feat: move the react-basic-example folder into the examples folder

--- react-basic-example/src/components/index.tsx
+++
@@ -1,5 +0,0 @@
-export { Avatar } from "./Avatar";
-export { Gallery } from "./Gallery";
-export { Button } from "./Button";
-export { List } from "./List";
-export { TeaSet } from "./Pure";
--- react-basic-example/src/utils.ts
+++
@@ -1,8 +0,0 @@
-export interface Person {
-  name: string;
-  imageId: string;
-}
-
-export const getImageUrl = (person: Person, size = "s") => {
-  return "https://i.imgur.com/" + person.imageId + size + ".jpg";
-};
