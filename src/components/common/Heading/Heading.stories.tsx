2073b202d4aa4a4880755a5c4a24263c27473412
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-24 18:09:06 +0700

    feat: implement storybook for heading

---
+++ src/components/common/Heading/Heading.stories.tsx
@@ -0,0 +1,13 @@
+import Heading from '.';
+
+export default {
+  component: Heading,
+  title: 'Heading',
+  tags: ['autodocs'],
+};
+
+export const Tables = {
+  args: {
+    text: 'Authors Table',
+  },
+};
