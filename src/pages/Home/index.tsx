e4550254f43ba8d00b34fe03634757c8c241a7cd
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-24 10:42:13 +0700

    feat: use template to create structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Authors Table</h1>
+      <button className="bg-primary border-2 border-primary">Add new author</button>
+    </>
+  );
+};
+
+export default Home;
