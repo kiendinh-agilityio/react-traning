f27387a6457f94c85a005c6a5dc7d6d087329ca1
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-24 10:33:09 +0700

    feat: install and config for tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4fd1c5',
+      },
+    },
+  },
+  plugins: [],
+};
