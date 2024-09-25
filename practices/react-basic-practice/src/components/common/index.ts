57f4e1f1aa6c5562fa35c8c106fcfdb183ba8b48
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-25 15:35:45 +0700

    feat: implement component input

--- practices/react-basic-practice/src/components/common/index.ts
+++ practices/react-basic-practice/src/components/common/index.ts
@@ -1,2 +1,4 @@
 export { default as Logo } from './Logo';
 export { default as Heading } from './Heading';
+export { default as SearchInput } from './SearchInput';
+export { default as InputField } from './InputField';
--- practices/react-basic-practice/src/pages/Home/index.tsx
+++ practices/react-basic-practice/src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
-import { Logo, Heading } from '@/components/common';
+import { Logo, Heading, SearchInput } from '@/components/common';
 
 const Home = () => {
   return (
     <>
       <Logo />
       <Heading text="Authors Table" />
+      <SearchInput />
       <button className="bg-primary border-2 border-primary">Add new author</button>
     </>
   );
