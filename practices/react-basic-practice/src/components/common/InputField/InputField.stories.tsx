f177afb875f50fda3446f15b72156429c156973f
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-25 15:35:26 +0700

    feat: implement storybook input field

---
+++ practices/react-basic-practice/src/components/common/InputField/InputField.stories.tsx
@@ -0,0 +1,23 @@
+import InputField from '.';
+
+export default {
+  component: InputField,
+  title: 'Input Field',
+  tags: ['autodocs'],
+};
+
+export const InputName = {
+  args: {
+    type: 'text',
+    name: 'name',
+    value: 'Esthera Jackson',
+  },
+};
+
+export const InputEmail = {
+  args: {
+    type: 'text',
+    name: 'email',
+    value: 'esthera@simmmple.com',
+  },
+};
