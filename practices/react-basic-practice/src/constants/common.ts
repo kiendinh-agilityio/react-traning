4706fef9c2d57fc874c10d2194c60457547723a0
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-25 17:44:14 +0700

    feat: implement constants for button styles

---
+++ practices/react-basic-practice/src/constants/common.ts
@@ -0,0 +1,10 @@
+export const BUTTON_STYLE = {
+  TEXT_COLOR: {
+    PRIMARY: 'text-primary',
+    LIGHT: 'text-white',
+  },
+  BACKGROUND_COLOR: {
+    PRIMARY: 'bg-primary',
+    SECONDARY: 'bg-transparent',
+  },
+};
---
+++ practices/react-basic-practice/src/constants/index.ts
@@ -0,0 +1,1 @@
+export { BUTTON_STYLE } from './common';
