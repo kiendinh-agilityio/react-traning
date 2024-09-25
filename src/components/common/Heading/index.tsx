03001ce2088460f92e3000b880c9c3e63faab8a4
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-25 09:53:01 +0700

    feat: config tailwind for storybook

--- src/components/common/Heading/index.tsx
+++ src/components/common/Heading/index.tsx
@@ -3,7 +3,9 @@ interface HeadingProps {
 }
 
 const Heading = ({ text }: HeadingProps) => (
-  <h1 className="font-helveticaBold text-dark text-[18px] leading-[25px] font-bold">{text}</h1>
+  <h1 className="flex justify-center font-helveticaBold text-dark text-[18px] leading-[25px] font-bold">
+    {text}
+  </h1>
 );
 
 export default Heading;
