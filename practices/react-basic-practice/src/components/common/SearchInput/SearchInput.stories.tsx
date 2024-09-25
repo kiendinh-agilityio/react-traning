816555d5ea5a1a4905ba942e5596931f1274ee1b
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-25 15:34:30 +0700

    feat: implement storybook search input

---
+++ practices/react-basic-practice/src/components/common/SearchInput/SearchInput.stories.tsx
@@ -0,0 +1,11 @@
+import SearchInput from '.';
+
+export default {
+  component: SearchInput,
+  title: 'Input Search',
+  tags: ['autodocs'],
+};
+
+export const SampleSearchBar = {
+  args: {},
+};
