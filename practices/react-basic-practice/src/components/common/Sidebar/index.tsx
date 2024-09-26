22ffdeddac8df92edcacc7046ce9acbe20f69489
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-26 17:00:34 +0700

    feat: implement component sidebar

---
+++ practices/react-basic-practice/src/components/common/Sidebar/index.tsx
@@ -0,0 +1,34 @@
+import { SVGProps } from 'react';
+import { SIDEBAR_ITEMS } from '@/constants';
+import SidebarItem from './SidebarItem';
+
+interface SidebarItemType {
+  icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  text: string;
+  active: boolean;
+  group?: string;
+}
+
+const renderSidebarItems = (items: SidebarItemType[]) =>
+  items.map((item) => (
+    <SidebarItem key={item.text} icon={item.icon} text={item.text} active={item.active} />
+  ));
+
+const Sidebar = () => {
+  const mainItems: SidebarItemType[] = SIDEBAR_ITEMS.filter((item) => !item.group);
+
+  // Filter out items belonging to the ACCOUNT PAGES group
+  const accountItems: SidebarItemType[] = SIDEBAR_ITEMS.filter(
+    (item) => item.group === 'ACCOUNT PAGES',
+  );
+
+  return (
+    <ul className="px-[30px]">
+      {renderSidebarItems(mainItems)}
+      <li className="font-bold font-helveticaBold py-6 px-4">ACCOUNT PAGES</li>
+      {renderSidebarItems(accountItems)}
+    </ul>
+  );
+};
+
+export default Sidebar;
--- practices/react-basic-practice/src/components/common/index.ts
+++ practices/react-basic-practice/src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as Logo } from './Logo';
 export { default as Heading } from './Heading';
 export { default as Input } from './Input';
 export { default as Button } from './Button';
+export { default as Sidebar } from './Sidebar';
