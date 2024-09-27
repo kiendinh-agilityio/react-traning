10d2e81f3065bd423fb479bf7006d215bb78559c
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-27 11:35:38 +0700

    feat: create constants for position list

---
+++ practices/react-basic-practice/src/constants/common.ts
@@ -0,0 +1,19 @@
+// Variables for option class list
+export const POSITION_LIST = [
+  {
+    name: 'Manager',
+    value: 'manager',
+  },
+  {
+    name: 'Programmer',
+    value: 'programmer',
+  },
+  {
+    name: 'Executive',
+    value: 'executive',
+  },
+  {
+    name: 'Designer',
+    value: 'designer',
+  },
+];
