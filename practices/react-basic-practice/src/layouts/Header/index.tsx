cd331c557e263f3cf4133731723fc0243081bc6d
Kien Dinh <kien.dinh@asnet.com.vn>
2024-09-30 22:41:26 +0700

    feat: implement for header

---
+++ practices/react-basic-practice/src/layouts/Header/index.tsx
@@ -0,0 +1,26 @@
+// Import components common icon
+import { NotificationIcon, PinionIcon, UserIcon } from '@/components/common/Icons';
+
+interface HeaderProps {
+  currentPage: string;
+}
+
+const Header = ({ currentPage }: HeaderProps) => {
+  return (
+    <header className="flex justify-between mb-7 px-[21px]">
+      <div>
+        <p className="text-[#a0aec0] mb-1.5">
+          Pages / <span className="text-dark">{currentPage}</span>
+        </p>
+        <p className="font-helveticaBold font-bold text-dark">{currentPage}</p>
+      </div>
+      <div className="flex items-center gap-[17px]">
+        <UserIcon />
+        <PinionIcon />
+        <NotificationIcon />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
