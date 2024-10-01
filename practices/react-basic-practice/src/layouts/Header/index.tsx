90659e041908afc196c425d27222cc2ca31485d8
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-01 09:53:34 +0700

    feat: update layouts for header

--- practices/react-basic-practice/src/layouts/Header/index.tsx
+++ practices/react-basic-practice/src/layouts/Header/index.tsx
@@ -5,22 +5,20 @@ interface HeaderProps {
   currentPage: string;
 }
 
-const Header = ({ currentPage }: HeaderProps) => {
-  return (
-    <header className="flex justify-between mb-7 px-[21px]">
-      <div>
-        <p className="text-[#a0aec0] mb-1.5">
-          Pages / <span className="text-dark">{currentPage}</span>
-        </p>
-        <p className="font-helveticaBold font-bold text-dark">{currentPage}</p>
-      </div>
-      <div className="flex items-center gap-[17px]">
-        <UserIcon />
-        <PinionIcon />
-        <NotificationIcon />
-      </div>
-    </header>
-  );
-};
+const Header = ({ currentPage }: HeaderProps) => (
+  <header className="flex justify-between mb-7 px-[21px]">
+    <div>
+      <p className="text-[#a0aec0] mb-1.5">
+        Pages / <span className="text-dark">{currentPage}</span>
+      </p>
+      <p className="font-helveticaBold font-bold text-dark">{currentPage}</p>
+    </div>
+    <div className="flex items-center gap-[17px]">
+      <UserIcon />
+      <PinionIcon />
+      <NotificationIcon />
+    </div>
+  </header>
+);
 
 export default Header;
