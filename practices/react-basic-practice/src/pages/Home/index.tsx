aabdfbbdd52806c9cc91103e3892204b1f591c31
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-01 22:51:45 +0700

    feat: implement display a list authors in a table

--- practices/react-basic-practice/src/pages/Home/index.tsx
+++ practices/react-basic-practice/src/pages/Home/index.tsx
@@ -1,13 +1,44 @@
+import { useEffect, useState } from 'react';
+
 // Import icons
 import { SearchIcon } from '@/components/common/Icons';
 
 // Import components
-import { Logo, Heading, Input, Button, Sidebar } from '@/components/common';
+import { Logo, Heading, Input, Button, Sidebar, LoadingSpinner } from '@/components/common';
+
+// Import components
+import { AuthorsTable } from '@/components';
 
 // Import layouts
 import { Header, Footer } from '@/layouts';
 
+// Import services
+import { getAllAuthors } from '@/services';
+
+// Import types
+import { Author } from '@/types';
+
 const Home = () => {
+  const [authors, setAuthors] = useState<Author[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  // Fetch authors on component mount
+  useEffect(() => {
+    const fetchAuthors = async () => {
+      setLoading(true);
+      try {
+        const authorsData = await getAllAuthors();
+        setAuthors(authorsData);
+      } catch (err) {
+        setError('Failed to load authors.');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchAuthors();
+  }, []);
+
   return (
     <>
       <div className="min-h-screen flex pt-[30px] pr-[22px] pb-[23px]">
@@ -18,7 +49,7 @@ const Home = () => {
         <div className="flex flex-col justify-between w-full">
           <Header currentPage="Tables" />
           <div className="bg-white min-h-screen mb-7 rounded-[15px] px-[21px] py-7">
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center mb-7">
               <Heading text="Authors Table" />
               <div className="flex gap-5">
                 <div className="w-96">
@@ -32,6 +63,15 @@ const Home = () => {
                 <Button variant="secondary" label="Add New Author" />
               </div>
             </div>
+            {loading ? (
+              <div className="flex justify-center">
+                <LoadingSpinner />
+              </div>
+            ) : error ? (
+              <p className="text-red-500">{error}</p>
+            ) : (
+              <AuthorsTable authors={authors} />
+            )}
           </div>
           <Footer />
         </div>
