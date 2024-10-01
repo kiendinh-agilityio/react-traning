887ff506714cacd4314ce2be31b86c51295ec212
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-01 09:53:21 +0700

    feat: update layouts for footer

--- practices/react-basic-practice/src/layouts/Footer/index.tsx
+++ practices/react-basic-practice/src/layouts/Footer/index.tsx
@@ -1,25 +1,25 @@
 // Import constants
 import { NAVBAR_ITEMS } from '@/constants';
 
-const Footer = () => {
-  return (
-    <footer className="flex justify-between text-sidebar px-[21px]">
-      <p>
-        @ 2021, Made with ❤️ by{' '}
-        <span className="text-primary font-helveticaBold font-bold">Creative Tim</span> &{' '}
-        <span className="text-primary font-helveticaBold font-bold">Simmmple</span> for a better web
-      </p>
-      <nav>
-        <ul className="flex gap-11">
-          {NAVBAR_ITEMS.map((item, index) => (
-            <li key={index}>
-              <a href="#">{item.label}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className="flex justify-between text-sidebar px-[21px]">
+    <p>
+      @ 2024, Made with ❤️ by{' '}
+      <span className="text-primary font-helveticaBold font-bold">Creative Tim</span> &{' '}
+      <span className="text-primary font-helveticaBold font-bold">Simmmple</span> for a better web
+    </p>
+    <nav>
+      <ul className="flex gap-11">
+        {NAVBAR_ITEMS.map((item) => (
+          <li key={item.href}>
+            <a href={item.href} target="_blank" rel="noopener noreferrer">
+              {item.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </footer>
+);
 
 export default Footer;
