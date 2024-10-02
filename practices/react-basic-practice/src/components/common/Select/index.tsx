d3e353f5036fd5514dd611b94aeea421f2f1139c
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-02 23:48:43 +0700

    feat: update components select

--- practices/react-basic-practice/src/components/common/Select/index.tsx
+++ practices/react-basic-practice/src/components/common/Select/index.tsx
@@ -1,12 +1,16 @@
+import { ChangeEvent } from 'react';
+
 type OptionsType = { text: string; value: string }[];
 
 interface SelectProps {
   label?: string;
   name: string;
   optionsList: OptionsType;
+  value?: string;
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
 
-const Select = ({ name, optionsList, label }: SelectProps) => {
+const Select = ({ name, optionsList, label, value, onChange }: SelectProps) => {
   return (
     <div className="flex flex-col gap-1.5 font-helveticaRegular text-dark font-regular">
       {label && <label>{label}</label>}
@@ -14,6 +18,8 @@ const Select = ({ name, optionsList, label }: SelectProps) => {
         className="px-5 py-[15px] border border-input w-full rounded-[15px] focus:outline-none"
         name={name}
         id={name}
+        value={value}
+        onChange={onChange}
       >
         {optionsList.map((option) => (
           <option key={option.value} value={option.value}>
