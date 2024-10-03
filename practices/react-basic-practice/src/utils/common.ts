c6964a73300c16bf26c0b7cefc131fae2e0c5706
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-03 17:40:05 +0700

    feat: update utils common

--- practices/react-basic-practice/src/utils/common.ts
+++ practices/react-basic-practice/src/utils/common.ts
@@ -1,3 +1,6 @@
+// Import types for Author
+import { Author } from '@/types';
+
 // Get the current year
 export const currentYear = new Date().getFullYear();
 
@@ -6,4 +9,16 @@ export const formatDate = (dateString: string): string => {
   const date = new Date(dateString);
 
   return date.toLocaleDateString();
+};
+
+// Define default author object for reuse
+export const profileAuthor: Author = {
+  id: '',
+  name: '',
+  email: '',
+  avatarUrl: '',
+  position: '',
+  roles: '',
+  status: '',
+  date: '',
 };
