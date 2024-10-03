c6ec132107d6a16ea37bb36cac60165e1ba6e627
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-03 14:50:42 +0700

    feat: update components select

--- practices/react-basic-practice/src/components/common/Select/index.tsx
+++ practices/react-basic-practice/src/components/common/Select/index.tsx
@@ -8,18 +8,29 @@ interface SelectProps {
   optionsList: OptionsType;
   value?: string;
   onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+  onBlur?: () => void;
+  errorMessage?: string;
 }
 
-const Select = ({ name, optionsList, label, value, onChange }: SelectProps) => {
+const Select = ({
+  name,
+  optionsList,
+  label,
+  value,
+  onChange,
+  errorMessage,
+  onBlur,
+}: SelectProps) => {
   return (
     <div className="flex flex-col gap-1.5 font-helveticaRegular text-dark font-regular">
       {label && <label>{label}</label>}
       <select
-        className="px-5 py-[15px] border border-input w-full rounded-[15px] focus:outline-none"
+        className={`px-5 py-[15px] border border-input w-full rounded-[15px] focus:outline-none mb-1.5 ${errorMessage ? 'border-danger' : ''}`}
         name={name}
         id={name}
         value={value}
         onChange={onChange}
+        onBlur={onBlur}
       >
         {optionsList.map((option) => (
           <option key={option.value} value={option.value}>
