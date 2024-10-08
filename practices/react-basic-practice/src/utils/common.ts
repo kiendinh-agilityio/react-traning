a3f14a10ac597b4b3ce514698ffcce6b449f5c51
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-08 16:33:29 +0700

    feat: use dayjs for function formatDate

--- practices/react-basic-practice/src/utils/common.ts
+++ practices/react-basic-practice/src/utils/common.ts
@@ -1,28 +1,13 @@
+// Import dayjs
+import dayjs from 'dayjs';
+
 // Import types for Author
 import { Author } from '@/types';
 
 // Get the current year
 export const currentYear = new Date().getFullYear();
 
-// Helper function to pad numbers to two digits
-const formatToTwoDigits = (num: number): string => String(num).padStart(2, '0');
-
-// Format date
-export const formatDate = (date: string): string => {
-  const parsedDate = new Date(date);
-
-  // Make sure there are 2 digits for the date
-  const day = formatToTwoDigits(parsedDate.getDate());
-
-  // Month starts at 0, add 1
-  const month = formatToTwoDigits(parsedDate.getMonth() + 1);
-
-  // Get the last 2 digits of the year
-  const year = String(parsedDate.getFullYear()).slice(-2);
-
-  // Returns the format dd/mm/yy
-  return `${day}/${month}/${year}`;
-};
+export const formatDate = (date: string): string => dayjs(date).format('DD/MM/YY');
 
 // Define default author object for reuse
 export const profileAuthor: Author = {
