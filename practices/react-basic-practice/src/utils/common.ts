ace001746ffe56ea2171aa898cf1991c70d06018
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-08 15:21:38 +0700

    feat: update formatDat for utils

--- practices/react-basic-practice/src/utils/common.ts
+++ practices/react-basic-practice/src/utils/common.ts
@@ -4,11 +4,24 @@ import { Author } from '@/types';
 // Get the current year
 export const currentYear = new Date().getFullYear();
 
-// Format date
-export const formatDate = (dateString: string): string => {
-  const date = new Date(dateString);
+// Helper function to pad numbers to two digits
+const formatToTwoDigits = (num: number): string => String(num).padStart(2, '0');
 
-  return date.toLocaleDateString();
+// Format date
+export const formatDate = (date: string): string => {
+  const parsedDate = new Date(date);
+
+  // Make sure there are 2 digits for the date
+  const day = formatToTwoDigits(parsedDate.getDate());
+
+  // Month starts at 0, add 1
+  const month = formatToTwoDigits(parsedDate.getMonth() + 1);
+
+  // Get the last 2 digits of the year
+  const year = String(parsedDate.getFullYear()).slice(-2);
+
+  // Returns the format dd/mm/yy
+  return `${day}/${month}/${year}`;
 };
 
 // Define default author object for reuse
