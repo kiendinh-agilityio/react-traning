e7148f611e47d55517e76339f2446664215899a2
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-08 15:28:14 +0700

    feat: update ui ux for modal confirm

--- practices/react-basic-practice/src/components/ConfirmModal/index.tsx
+++ practices/react-basic-practice/src/components/ConfirmModal/index.tsx
@@ -1,5 +1,6 @@
 // Import components
 import { Button } from '@/components/common/';
+import { TrashIcon, CloseIcon } from '@/components/common/Icons';
 
 interface ConfirmModalProps {
   onSubmit: () => void;
@@ -7,13 +8,17 @@ interface ConfirmModalProps {
 }
 
 const ConfirmModal = ({ onSubmit, onClose }: ConfirmModalProps) => (
-  <div className="flex flex-col gap-10 justify-center items-center p-6">
-    <h2 className="font-helveticaBold text-3xl font-bold text-center text-[#a0aec0]">
+  <div className="flex flex-col gap-8 justify-center items-center py-4 relative">
+    <TrashIcon />
+    <button className="absolute right-0 top-0" onClick={onClose}>
+      <CloseIcon />
+    </button>
+    <h2 className="font-helveticaRegular text-2xl font-regular text-center text-dark">
       Are you sure you want to delete this author?
     </h2>
     <div className="flex justify-center items-center gap-6">
-      <Button variant="primary" label="Confirm" onClick={onSubmit} />
-      <Button variant="secondary" label="Cancel" onClick={onClose} />
+      <Button variant="danger" label="Yes, I'm sure" onClick={onSubmit} />
+      <Button variant="tertiary" label="No, Cancel" onClick={onClose} />
     </div>
   </div>
 );
