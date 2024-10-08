352764da4c5bfe5587deaf6ed7e3cc10a0a76792
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-08 16:30:30 +0700

    feat: update components modal

--- practices/react-basic-practice/src/components/common/Modal/index.tsx
+++ practices/react-basic-practice/src/components/common/Modal/index.tsx
@@ -12,6 +12,9 @@ const Modal = ({ children, className, onClose }: ModalProps) => {
     onClose();
   };
 
+  const stopPropagation = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) =>
+    event.stopPropagation();
+
   return (
     <div
       className="modal bg-overlay fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center"
@@ -19,7 +22,7 @@ const Modal = ({ children, className, onClose }: ModalProps) => {
     >
       <div
         className={`flex flex-col gap-8 rounded-2xl bg-white shadow z-10 ${className}`}
-        onClick={(e) => e.stopPropagation()}
+        onClick={stopPropagation}
       >
         {children}
       </div>
