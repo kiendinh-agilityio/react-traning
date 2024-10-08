66088284139c831c60b1fdb4a34e05d5465c21ac
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-08 16:54:01 +0700

    feat: create constants for date format

--- practices/react-basic-practice/src/utils/common.ts
+++ practices/react-basic-practice/src/utils/common.ts
@@ -4,10 +4,14 @@ import dayjs from 'dayjs';
 // Import types for Author
 import { Author } from '@/types';
 
+// Import constants
+import { DATE_FORMAT } from '@/constants';
+
 // Get the current year
 export const currentYear = new Date().getFullYear();
 
-export const formatDate = (date: string, dateFormat: string = 'DD/MM/YY'): string =>
+// Function format date
+export const formatDate = (date: string, dateFormat: string = DATE_FORMAT): string =>
   dayjs(date).format(dateFormat);
 
 // Define default author object for reuse
