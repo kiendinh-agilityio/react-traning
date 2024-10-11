1951233d21dbbf699a8ee8b726b3e6b3479cbb0a
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-11 10:18:27 +0700

    feat: update padding for author item

--- practices/react-basic-practice/src/components/AuthorsTable/AuthorItem/index.tsx
+++ practices/react-basic-practice/src/components/AuthorsTable/AuthorItem/index.tsx
@@ -28,7 +28,7 @@ const AuthorItem = ({ author, onEdit, onDelete }: AuthorItemProps) => {
   return (
     <div key={id} className="flex items-center border-b border-input text-sm">
       {/* Author Info */}
-      <div className="flex-1 py-3.5">
+      <div className="flex-1 py-[11px]">
         <div className="flex items-center">
           <img className="w-10 h-10 rounded-[12px] mr-3.5" src={avatarUrl} alt={`${name} avatar`} />
           <div>
@@ -41,13 +41,13 @@ const AuthorItem = ({ author, onEdit, onDelete }: AuthorItemProps) => {
       </div>
 
       {/* Function */}
-      <div className="flex-1 py-3.5">
+      <div className="flex-1 py-[11px]">
         <p className="font-helveticaBold font-bold">{roles}</p>
         <p className="text-gray">{position}</p>
       </div>
 
       {/* Status */}
-      <div className="flex-1 py-3.5">
+      <div className="flex-1 py-[11px]">
         <div
           className={`flex justify-center items-center w-[70px] h-[25px] border rounded-lg font-helveticaBold font-bold text-white ${
             status === 'Active' ? 'border-active bg-active' : 'border-inactive bg-inactive'
@@ -58,15 +58,15 @@ const AuthorItem = ({ author, onEdit, onDelete }: AuthorItemProps) => {
       </div>
 
       {/* Employed Date */}
-      <div className="flex-1 py-3.5 font-helveticaBold font-bold">
+      <div className="flex-1 py-[11px] font-helveticaBold font-bold">
         {formatDate(date, DATE_FORMAT)}
       </div>
 
       {/* Actions */}
-      <div className="w-32 flex justify-between items-center font-helveticaBold font-bold">
+      <div className="w-32 flex justify-between items-center font-helveticaBold font-bold text-xs leading-[18px]">
         {/* Button Edit */}
         <button className="flex items-center gap-0.5" onClick={handleEditClick}>
-          <EditIcon /> <span>Edit</span>
+          <EditIcon /> <span className="text-[#718096]">Edit</span>
         </button>
 
         {/* Button Delete */}
