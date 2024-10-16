27630dd003b2cd165681696ac88c1135fa195753
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-16 15:31:47 +0700

    feat: update variant for button components

--- practices/react-basic-practice/src/components/common/Button/index.tsx
+++ practices/react-basic-practice/src/components/common/Button/index.tsx
@@ -1,5 +1,5 @@
 interface ButtonProps {
-  variant?: 'primary' | 'secondary' | 'tertiary' | 'danger';
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'danger' | 'normal';
   isDisabled?: boolean;
   onClick?: () => void;
   label: string;
@@ -7,10 +7,9 @@ interface ButtonProps {
 
 const Button = ({ variant = 'primary', isDisabled, onClick, label }: ButtonProps) => {
   // Conditional class name based on variant and disabled state
-  const className = `
-    ${isDisabled ? 'primary opacity-50 cursor-not-allowed' : variant} 
+  const className = ` 
     w-40 px-[22px] py-2.5 border border-primary 
-    font-helveticaBold font-bold rounded-xl
+    font-helveticaBold font-bold rounded-xl ${isDisabled ? 'primary opacity-50 cursor-not-allowed' : variant}
   `;
 
   return (
