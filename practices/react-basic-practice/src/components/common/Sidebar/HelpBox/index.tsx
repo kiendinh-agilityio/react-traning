15fe4e3aaad67838e58b66415194e53cf4a8a8e0
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-22 17:40:21 +0700

    feat: update component helpbox

--- practices/react-basic-practice/src/components/common/Sidebar/HelpBox/index.tsx
+++ practices/react-basic-practice/src/components/common/Sidebar/HelpBox/index.tsx
@@ -2,16 +2,16 @@
 import { QuestionIcon } from '@/components/common/Icons';
 
 // Import common components button
-import { Button } from '@/components/common/';
+import { Button, Paragraph } from '@/components/common/';
 
 const HelpBox = () => (
   <div className="bg-primary w-[218px] py-4 px-4 border border-primary rounded-[15px] m-auto mt-16 text-white">
     <div className="flex items-center justify-center w-[35px] h-[35px] bg-white rounded-xl mb-5">
       <QuestionIcon />
     </div>
-    <p className="font-helveticaBold font-bold tracking-wide">Need help?</p>
-    <p className="font-xs leading-base mb-[9px]">Please check our docs</p>
-    <Button variant="normal" label="DOCUMENTATION" />
+    <Paragraph variant="bold" size="sm" text="Need help?" />
+    <Paragraph variant="regular" size="xs" text="Please check our docs" className="mb-[9px]" />
+    <Button variant="link" label="DOCUMENTATION" />
   </div>
 );
 
