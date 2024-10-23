52e4eb7d64fa5210213135a89113c6af6fb4faa6
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-23 11:34:24 +0700

    feat: create component InputGroup

---
+++ practices/react-basic-practice/src/components/common/InputGroup/index.tsx
@@ -0,0 +1,42 @@
+// Import components
+import { Input } from '@/components/common';
+
+interface InputGroupProps {
+  label?: string;
+  name: string;
+  type: string;
+  value: string;
+  placeholder?: string;
+  errorMessage: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+}
+
+const InputGroup = ({
+  label,
+  name,
+  type,
+  value,
+  placeholder,
+  errorMessage,
+  onChange,
+  onBlur,
+}: InputGroupProps) => {
+  return (
+    <div className="flex flex-col gap-1.5">
+      <label>{label}</label>
+      <Input
+        name={name}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        onBlur={onBlur}
+        errorMessage={errorMessage}
+      />
+      {errorMessage && <p className="text-danger">{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default InputGroup;
