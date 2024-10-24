8c4ece77a26f2253482e358e1a37467d69972dd6
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-24 14:49:39 +0700

    feat: update component sidebar item

--- practices/react-basic-practice/src/components/common/Sidebar/SidebarItem/index.tsx
+++ practices/react-basic-practice/src/components/common/Sidebar/SidebarItem/index.tsx
@@ -1,3 +1,5 @@
+import { IconWrapper } from '@/components/common/';
+
 interface SidebarItemProps {
   icon: React.ReactNode;
   label: string;
@@ -7,14 +9,10 @@ interface SidebarItemProps {
 const SidebarItem = ({ icon, label, active }: SidebarItemProps) => {
   const className = `sidebar-item ${active ? 'bg-white text-dark w-[220px] h-[54px] flex items-center rounded-[15px] cursor-pointer' : ''}`;
 
-  const iconClassName = `flex items-center justify-center w-[30px] h-[30px] rounded-xl border drop-shadow-[0px_3.5px_5.5px_0px_#00000005] ${
-    active ? 'bg-primary border-primary' : 'bg-white border-white'
-  }`;
-
   return (
     <li className={className}>
       <a className="flex items-center gap-4" href="#">
-        <span className={iconClassName}>{icon}</span>
+        <IconWrapper icon={icon} active={active} />
         <span className="text-xs leading-base">{label}</span>
       </a>
     </li>
