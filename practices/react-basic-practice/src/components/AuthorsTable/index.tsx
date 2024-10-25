fac739ab48f5a93e628c18bd4f66deff377b705a
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-25 16:53:01 +0700

    feat: update components AuthorsTable

--- practices/react-basic-practice/src/components/AuthorsTable/index.tsx
+++ practices/react-basic-practice/src/components/AuthorsTable/index.tsx
@@ -13,23 +13,35 @@ export interface AuthorTableProps {
   onDeleteAuthor: (id: string) => void;
 }
 
-const AuthorTable = ({ authors, onEditAuthor, onDeleteAuthor }: AuthorTableProps) => (
-  <div className="w-full">
-    {/* Header */}
-    <ul className="flex border-b border-input font-helveticaBold font-bold text-[#a0aec0]">
-      {TABLE_TITLES.map((title) => (
-        <li key={title} className={`flex-1 py-3.5`}>
-          {title}
-        </li>
-      ))}
-      <li className="w-32 py-3.5"></li>
-    </ul>
+const AuthorTable = ({ authors, onEditAuthor, onDeleteAuthor }: AuthorTableProps) => {
+  const renderTableHeader = () =>
+    TABLE_TITLES.map((title) => (
+      <li key={title} className="flex-1 py-3.5">
+        {title}
+      </li>
+    ));
 
-    {/* Body */}
-    {authors.map((author) => (
+  const renderTableBody = (
+    authors: Author[],
+    onEditAuthor: (updatedAuthor: Author) => void,
+    onDeleteAuthor: (id: string) => void,
+  ) =>
+    authors.map((author) => (
       <AuthorItem key={author.id} author={author} onEdit={onEditAuthor} onDelete={onDeleteAuthor} />
-    ))}
-  </div>
-);
+    ));
+
+  return (
+    <div className="w-full">
+      {/* Header */}
+      <ul className="flex border-b border-input font-helveticaBold font-bold text-[#a0aec0]">
+        {renderTableHeader()}
+        <li className="w-32 py-3.5"></li>
+      </ul>
+
+      {/* Body */}
+      {renderTableBody(authors, onEditAuthor, onDeleteAuthor)}
+    </div>
+  );
+};
 
 export default AuthorTable;
