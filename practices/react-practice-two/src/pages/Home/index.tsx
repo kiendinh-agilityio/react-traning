deb259e40d133b83c5c30143360ceed5c15323a7
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-29 17:29:55 +0700

    feat: init structure for practice two

---
+++ practices/react-practice-two/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => (
+  <>
+    <h1 className="font-bold text-primary text-[90px] leading-[95px]">
+      <span className="font-light">LET'S RIDE</span> <br />
+      THE FUTURE.
+    </h1>
+  </>
+);
+
+export default Home;
