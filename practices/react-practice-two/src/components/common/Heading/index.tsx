7804dbc78aa8c306fd03285f21d8b921d9030ab1
Kien Dinh <kien.dinh@asnet.com.vn>
2024-10-31 15:19:54 +0700

    feat: update component Heading

--- practices/react-practice-two/src/components/common/Heading/index.tsx
+++ practices/react-practice-two/src/components/common/Heading/index.tsx
@@ -5,20 +5,13 @@ interface HeadingProps {
   level?: 1 | 2 | 3;
   size?: TextSize;
   spanContent?: string;
-  className?: string;
 }
 
-const Heading = ({
-  level = 2,
-  size = TextSize.Large,
-  text,
-  spanContent,
-  className,
-}: HeadingProps) => {
+const Heading = ({ level = 2, size = TextSize.Large, text, spanContent }: HeadingProps) => {
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
 
   return (
-    <Tag className={`text-${size} font-bold text-primary ${className}`}>
+    <Tag className={`text-${size} font-bold text-primary`}>
       {spanContent && <span className="font-light">{spanContent}</span>} {text}
     </Tag>
   );
