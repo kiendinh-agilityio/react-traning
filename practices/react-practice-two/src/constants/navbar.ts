e6435e82b5041b193d00c7f11297d011d5d33922
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-01 16:32:30 +0700

    feat: create constants for navbar

---
+++ practices/react-practice-two/src/constants/navbar.ts
@@ -0,0 +1,10 @@
+export const NAVBAR_ITEMS = [
+  { id: 'products', label: 'PRODUCTS', hasDropdown: true },
+  { id: 'gallery', label: 'GALLERY', href: '/gallery', hasDropdown: false },
+  { id: 'contact', label: 'CONTACT', href: '/contact', hasDropdown: false },
+];
+
+export const DROPDOWN_PRODUCTS = [
+  { id: 'info', label: 'INFO', href: '/info' },
+  { id: 'colors', label: 'COLORS', href: '/colors' },
+];
