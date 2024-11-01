82c249cfe35a150a979e01ef3cb4e0a1a9a79a5d
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-01 16:32:00 +0700

    feat: add ArrowDownIcon for dropdown

--- practices/react-practice-two/src/components/common/Icons/index.ts
+++ practices/react-practice-two/src/components/common/Icons/index.ts
@@ -1,3 +1,4 @@
 export { default as ScooterIcon } from './ScooterIcon';
 export { default as LinkIcon } from './LinkIcon';
 export { default as PlayIcon } from './PlayIcon';
+export { default as ArrowDownIcon } from './ArrowDownIcon';
