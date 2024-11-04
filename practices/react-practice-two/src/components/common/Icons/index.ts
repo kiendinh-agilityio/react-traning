2c4149b580932f16e82e592b9f7e84162fc8eb8f
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-04 16:53:07 +0700

    feat: implement components common icons for social

--- practices/react-practice-two/src/components/common/Icons/index.ts
+++ practices/react-practice-two/src/components/common/Icons/index.ts
@@ -2,3 +2,6 @@ export { default as ScooterIcon } from './ScooterIcon';
 export { default as LinkIcon } from './LinkIcon';
 export { default as PlayIcon } from './PlayIcon';
 export { default as ArrowDownIcon } from './ArrowDownIcon';
+export { default as FacebookIcon } from './FacebookIcon';
+export { default as TwitterIcon } from './TwitterIcon';
+export { default as InstagramIcon } from './InstagramIcon';
