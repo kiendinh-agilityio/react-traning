f329067bdd2ea1f664fe111b26f90919fe0b720c
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-05 15:45:05 +0700

    feat: implement component FeatureItem for features section

---
+++ practices/react-practice-two/src/layouts/Sections/Features.tsx
@@ -0,0 +1,20 @@
+// import component
+import { FeatureItem } from '@/components/common';
+
+// import constants
+import { FEATURES_LIST } from '@/constants';
+
+const FeaturesSection = () => {
+  const renderFeaturesList = FEATURES_LIST.map((feature) => (
+    <FeatureItem
+      key={feature.id}
+      value={feature.value}
+      unit={feature.unit}
+      description={feature.description}
+    />
+  ));
+
+  return <section className="flex justify-between">{renderFeaturesList}</section>;
+};
+
+export default FeaturesSection;
