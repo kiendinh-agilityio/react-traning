36f0b2869f66f4fc81a8e1c42d60266d596afe30
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-05 17:45:42 +0700

    feat: implement component List

---
+++ practices/react-practice-two/src/components/common/List/index.tsx
@@ -0,0 +1,25 @@
+// import components
+import { Paragraph } from '@/components/common';
+
+interface ListItem {
+  id: number;
+  value: string;
+}
+
+interface ListProps {
+  items: ListItem[];
+  icon?: React.ReactNode;
+}
+
+const List = ({ items, icon }: ListProps) => {
+  const renderList = items.map((item) => (
+    <li key={item.id} className="flex items-center gap-[18px] text-primary">
+      {icon && icon}
+      <Paragraph text={item.value} />
+    </li>
+  ));
+
+  return <ul className="flex flex-col gap-[33px]">{renderList}</ul>;
+};
+
+export default List;
