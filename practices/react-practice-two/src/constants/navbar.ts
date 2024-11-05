cafce0adaf728807cd69a687516ee370c3b7536d
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-05 10:12:33 +0700

    feat: update constant for navbar

--- practices/react-practice-two/src/constants/navbar.ts
+++ practices/react-practice-two/src/constants/navbar.ts
@@ -1,10 +1,13 @@
 export const NAVBAR_ITEMS = [
-  { id: 'products', label: 'PRODUCTS', hasDropdown: true },
+  {
+    id: 'products',
+    label: 'PRODUCTS',
+    hasDropdown: true,
+    subNavbar: [
+      { id: 'info', label: 'INFO', href: '/info' },
+      { id: 'colors', label: 'COLORS', href: '/colors' },
+    ],
+  },
   { id: 'gallery', label: 'GALLERY', href: '/gallery', hasDropdown: false },
   { id: 'contact', label: 'CONTACT', href: '/contact', hasDropdown: false },
-];
-
-export const DROPDOWN_PRODUCTS = [
-  { id: 'info', label: 'INFO', href: '/info' },
-  { id: 'colors', label: 'COLORS', href: '/colors' },
 ];
