23dc7e0f73fd6177c14aa9f0a188cc28839c8ad2
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-07 14:32:23 +0700

    feat: create folder assets images

---
+++ practices/react-practice-two/src/assets/images/index.ts
@@ -0,0 +1,6 @@
+import BritainEriksen from './BritainEriksen.svg';
+import SerhiyHipskyy from './SerhiyHipskyy.svg';
+import JustusMenke from './JustusMenke.svg';
+import DefaultAvatar from './DefaultAvatar.svg';
+
+export { BritainEriksen, SerhiyHipskyy, JustusMenke, DefaultAvatar };
