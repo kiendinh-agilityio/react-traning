8b1abcbaf8572c119aefdab405622e0d4487811a
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-11 11:17:11 +0700

    feat: implement hero section

--- practices/react-practice-two/src/layouts/Sections/index.ts
+++ practices/react-practice-two/src/layouts/Sections/index.ts
@@ -1,3 +1,4 @@
 export { default as FeaturesSection } from './Features';
 export { default as TestimonialsSection } from './Testimonials';
 export { default as ProductColorsSection } from './ProductColors';
+export { default as HeroSection } from './Hero';
