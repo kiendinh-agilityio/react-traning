6caaf7de1a19fa7f88c5a070ce4e514afd67ab82
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-12 10:34:50 +0700

    feat: create component FooterLinkGroup

---
+++ practices/react-practice-two/src/layouts/Footer/FooterLinkGroup/index.tsx
@@ -0,0 +1,37 @@
+// Import components
+import { Link, Paragraph } from '@/components/common';
+
+// Import types
+import { ParagraphVariant, TextSize } from '@/types';
+
+interface LinkItem {
+  href: string;
+  text: string;
+}
+
+interface FooterLinkGroupProps {
+  title: string;
+  items: LinkItem[];
+}
+
+const FooterLinkGroup = ({ title, items }: FooterLinkGroupProps) => {
+  const renderFooterLinkGroup = items.map((item) => (
+    <li key={item.href}>
+      <Link href={item.href} target="_blank" text={item.text} />
+    </li>
+  ));
+
+  return (
+    <div className="text-light">
+      <Paragraph
+        variant={ParagraphVariant.Bold}
+        size={TextSize.Medium}
+        text={title}
+        className="leading-[25px]"
+      />
+      <ul className="mt-[35px]">{renderFooterLinkGroup}</ul>
+    </div>
+  );
+};
+
+export default FooterLinkGroup;
