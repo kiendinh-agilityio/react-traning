a0eaf26c0f1ef3f84eda127004bd4146578b4440
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-13 15:40:00 +0700

    feat: update path of components for high efficiency section

--- practices/react-practice-two/src/layouts/Sections/HighEfficiency.tsx
+++ practices/react-practice-two/src/layouts/Sections/HighEfficiency.tsx
@@ -1,11 +1,11 @@
 // import components
-import { Intro } from '@/components/common';
+import { Intro } from '@/components';
 
 // import image
 import { HighEfficiency } from '@/assets/images';
 
 const HighEfficiencySection = () => (
-  <section className="container py-16">
+  <section className="container pt-[56px] pb-16">
     <Intro
       title="High Efficiency Motor"
       description="More torque for powerful riding with 22% maximum hill climbing capability."
