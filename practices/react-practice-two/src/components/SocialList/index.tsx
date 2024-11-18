3a5a26761a2abdcf3176d5f0c09f1e39ba0206cb
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-18 16:11:09 +0700

    feat: update for components SocialList

--- practices/react-practice-two/src/components/SocialList/index.tsx
+++ practices/react-practice-two/src/components/SocialList/index.tsx
@@ -4,7 +4,7 @@ import SocialItem from './SocialItem';
 import { FacebookIcon, TwitterIcon, InstagramIcon } from '@/components/common/Icons';
 
 export const SocialList = () => (
-  <ul className="flex gap-4">
+  <ul className="flex gap-4 mt-[22px]">
     <SocialItem href="https://facebook.com" icon={<FacebookIcon />} />
     <SocialItem href="https://twitter.com" icon={<TwitterIcon />} />
     <SocialItem href="https://instagram.com" icon={<InstagramIcon />} />
