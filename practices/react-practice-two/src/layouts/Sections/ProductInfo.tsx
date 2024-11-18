4c0b0851617c9d8bd711ca3851c96d692cfc334d
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-18 16:16:45 +0700

    feat: update styles for ProductInfo section

--- practices/react-practice-two/src/layouts/Sections/ProductInfo.tsx
+++ practices/react-practice-two/src/layouts/Sections/ProductInfo.tsx
@@ -14,14 +14,16 @@ import { PRODUCT_INFO_LIST } from '@/constants';
 import { ProductInfo } from '@/assets/images';
 
 const ProductInfoSection = () => (
-  <section className="container py-16">
-    <Intro
-      title="Product Information"
-      description="Our Scooter has following unique design and technology features:"
-    />
-    <div className="flex items-center justify-between mt-[22px]">
-      <img src={ProductInfo} alt="Product Info" />
-      <List icon={<ProductIcon />} items={PRODUCT_INFO_LIST} />
+  <section className="py-16">
+    <div className="container">
+      <Intro
+        title="Product Information"
+        description="Our Scooter has following unique design and technology features:"
+      />
+      <div className="flex items-center justify-between mt-[22px]">
+        <img src={ProductInfo} alt="Product Info" />
+        <List icon={<ProductIcon />} items={PRODUCT_INFO_LIST} />
+      </div>
     </div>
   </section>
 );
