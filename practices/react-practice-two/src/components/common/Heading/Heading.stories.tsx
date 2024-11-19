a2dfae1a3f0c421fce9ad1d7370cc2863be9af2a
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-19 17:45:09 +0700

    feat: update storybook for heading

--- practices/react-practice-two/src/components/common/Heading/Heading.stories.tsx
+++ practices/react-practice-two/src/components/common/Heading/Heading.stories.tsx
@@ -18,9 +18,8 @@ export default {
 } as Meta;
 
 type HeadingStoryProps = {
-  text: string;
+  children: React.ReactNode;
   level: 1 | 2 | 3;
-  spanContent?: string;
 };
 
 const Template: StoryFn<HeadingStoryProps> = (args: HeadingStoryProps) => <Heading {...args} />;
@@ -29,13 +28,12 @@ const Template: StoryFn<HeadingStoryProps> = (args: HeadingStoryProps) => <Headi
 export const H1 = Template.bind({});
 H1.args = {
   level: 1,
-  text: 'THE FUTURE.',
-  spanContent: "LET'S RIDE",
+  children: 'THE FUTURE.',
 };
 
 // H2 Story
 export const H2 = Template.bind({});
 H2.args = {
-  text: 'Product Information',
+  children: 'Product Information',
   level: 2,
 };
