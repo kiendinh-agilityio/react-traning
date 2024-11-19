c83ce68f34c7c71d169fad0a3768d735f0fc8031
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-19 14:32:18 +0700

    feat: update styles for practice two

--- practices/react-practice-two/src/layouts/Sections/HighEfficiency.tsx
+++ practices/react-practice-two/src/layouts/Sections/HighEfficiency.tsx
@@ -5,14 +5,14 @@ import { Intro } from '@/components';
 import { HighEfficiency } from '@/assets/images';
 
 const HighEfficiencySection = () => (
-  <section className="pt-[56px] pb-16">
+  <section className="pt-[56px] pb-16 flex flex-col justify-center">
     <div className="container">
       <Intro
         title="High Efficiency Motor"
         description="More torque for powerful riding with 22% maximum hill climbing capability."
       />
-      <img src={HighEfficiency} alt="Product Info" />
     </div>
+    <img src={HighEfficiency} alt="Product Info" />
   </section>
 );
 
