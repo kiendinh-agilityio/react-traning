d55704699e764806efc234da15f49959129011c0
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-19 17:38:25 +0700

    feat: update props children for Link

--- practices/react-practice-two/src/components/common/Link/index.tsx
+++ practices/react-practice-two/src/components/common/Link/index.tsx
@@ -1,19 +1,17 @@
 interface LinkProps {
   href: string;
-  text?: string;
+  children: React.ReactNode;
   target?: '_blank' | '_self' | '_parent' | '_top';
-  icon?: React.ReactNode;
   className?: string;
 }
 
-const Link = ({ text, href, className = '', target = '_self', icon }: LinkProps) => (
+const Link = ({ children, href, className = '', target = '_self' }: LinkProps) => (
   <a
     href={href}
     className={`font-regular text-sm leading-[54px] ${className && className}`}
     target={target}
   >
-    {text && text}
-    {icon && icon}
+    {children}
   </a>
 );
 
