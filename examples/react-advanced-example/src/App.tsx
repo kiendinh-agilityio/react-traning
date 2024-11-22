33fd72912fe428476bac8a5cd8a303e633fa248a
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-22 17:17:17 +0700

    feat: init structure for react advandced example

---
+++ examples/react-advanced-example/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import { MarkdownEditor } from './components';
+
+const App = () => {
+  return (
+    <>
+      <h1>React Advanced Examples</h1>
+      <MarkdownEditor />
+    </>
+  );
+};
+
+export default App;
