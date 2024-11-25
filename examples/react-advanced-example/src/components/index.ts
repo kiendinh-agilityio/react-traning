86f87b6ead5d3abffb113863751516cd1e470e9e
Kien Dinh <kien.dinh@asnet.com.vn>
2024-11-25 17:34:25 +0700

    feat: update example ThemeProvider for useContext

--- examples/react-advanced-example/src/components/index.ts
+++ examples/react-advanced-example/src/components/index.ts
@@ -1,1 +1,7 @@
 export { default as MarkdownEditor } from './MarkdownEditor';
+export { default as Loading } from './Loading';
+export { default as ThemeProvider, ThemedComponent } from './ThemeProvider';
+export {
+  default as LanguageProvider,
+  LanguageSwitcher,
+} from './LanguageProvider';
