60e4e281e0f0aa26a306bd8a15bc32c0da13f087
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-07 17:15:37 +0700

    feat: implement component Sidebar

---
+++ practices/react-advanced-practice/src/components/Sidebar/index.tsx
@@ -0,0 +1,49 @@
+import { memo, useMemo } from 'react';
+
+// Import constants
+import { SIDEBAR_ITEMS } from '@/constants';
+
+// Import sidebar item
+import SidebarItem from './SidebarItem';
+
+interface SidebarItemType {
+  icon: React.ReactNode;
+  label: string;
+  active: boolean;
+  group?: string;
+}
+
+const Sidebar = () => {
+  const mainItems: SidebarItemType[] = useMemo(
+    () => SIDEBAR_ITEMS.filter((item) => !item.group),
+    [],
+  );
+
+  const accountItems: SidebarItemType[] = useMemo(
+    () => SIDEBAR_ITEMS.filter((item) => item.group === 'ACCOUNT PAGES'),
+    [],
+  );
+
+  // Function renderSidebarItems
+  const renderSidebarItems = (items: SidebarItemType[]) =>
+    items.map((item) => (
+      <SidebarItem
+        key={item.label}
+        icon={item.icon}
+        label={item.label}
+        active={item.active}
+      />
+    ));
+
+  return (
+    <>
+      <ul className="px-[30px]">
+        {renderSidebarItems(mainItems)}
+        <li className="font-bold font-helveticaBold py-6 px-4">ACCOUNT PAGES</li>
+        {renderSidebarItems(accountItems)}
+      </ul>
+    </>
+  );
+};
+
+export default memo(Sidebar);
