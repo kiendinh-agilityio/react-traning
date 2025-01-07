bfa030d8a13a8ca38f78e0a66758027e29d5e631
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-07 15:22:11 +0700

    feat: fix conflict

--- practices/react-advanced-practice/src/components/common/Icons/index.ts
+++ practices/react-advanced-practice/src/components/common/Icons/index.ts
@@ -1,1 +1,5 @@
 export { default as PurityIcon } from './PurityLogo';
+export { default as SearchIcon } from './SearchIcon';
+export { default as ShowPasswordIcon } from './ShowPassword';
+export { default as HidePasswordIcon } from './HidePassword';
+export { default as ArrowSelectIcon } from './ArrowSelect';
