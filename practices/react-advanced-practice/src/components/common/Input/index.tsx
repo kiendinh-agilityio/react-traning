32e7a6ec41dfaaab5928e23a8e73936d0ee5e35f
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-07 14:51:08 +0700

    feat: implement components Input

---
+++ practices/react-advanced-practice/src/components/common/Input/index.tsx
@@ -0,0 +1,59 @@
+import { ReactNode, ChangeEvent, forwardRef, memo } from 'react';
+
+// Import radix ui
+import { Flex } from '@radix-ui/themes';
+
+interface InputProps {
+  name: string;
+  placeholder: string;
+  value?: string;
+  type?: 'search' | 'text' | 'email' | 'password' | 'date';
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+  errorMessage?: string;
+  defaultValue?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+}
+
+const Input = memo(
+  forwardRef<HTMLInputElement, InputProps>(
+    (
+      {
+        value,
+        name,
+        placeholder,
+        type = 'text',
+        leftIcon,
+        rightIcon,
+        onChange,
+        onBlur,
+        errorMessage,
+        defaultValue,
+      },
+      ref,
+    ) => {
+      const className = `flex justify-center items-center px-5 py-[15px] border gap-3 w-full rounded-[15px] ${errorMessage ? 'border-danger' : 'border-input'}`;
+
+      return (
+        <Flex justify="center" align="center" className={className}>
+          {leftIcon && leftIcon}
+          <input
+            ref={ref}
+            type={type}
+            value={value}
+            defaultValue={defaultValue}
+            name={name}
+            placeholder={placeholder}
+            onChange={onChange}
+            onBlur={onBlur}
+            className="w-full text-regular focus:outline-none text-dark placeholder:text-base"
+          />
+          {rightIcon && rightIcon}
+        </Flex>
+      );
+    },
+  ),
+);
+
+export default Input;
