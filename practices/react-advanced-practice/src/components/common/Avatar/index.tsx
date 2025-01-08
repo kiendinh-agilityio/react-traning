d4c4bbc03621f4d6a6e857f57b72d0eaa027849e
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-08 15:10:48 +0700

    feat: update component Avatar

--- practices/react-advanced-practice/src/components/common/Avatar/index.tsx
+++ practices/react-advanced-practice/src/components/common/Avatar/index.tsx
@@ -1,5 +1,3 @@
-import { memo } from 'react';
-
 // import radix ui
 import { Avatar as AvatarBase, AvatarProps } from '@radix-ui/themes';
 
@@ -8,24 +6,20 @@ type AvatarBaseProps = {
   alt?: string;
 } & AvatarProps;
 
-const Avatar = memo(
-  ({
-    ariaLabel = 'Avatar',
-    alt = 'Avatar',
-    src = '',
-    className = '',
-    fallback,
-  }: AvatarBaseProps) => {
-    return (
-      <AvatarBase
-        aria-label={ariaLabel}
-        alt={alt}
-        src={src}
-        fallback={fallback}
-        className={`rounded-base w-[40px] h-[40px] ${className}`}
-      />
-    );
-  },
+const Avatar = ({
+  ariaLabel = 'Avatar',
+  alt = 'Avatar',
+  src = '',
+  className = '',
+  fallback,
+}: AvatarBaseProps) => (
+  <AvatarBase
+    aria-label={ariaLabel}
+    alt={alt}
+    src={src}
+    fallback={fallback}
+    className={`rounded-base w-[40px] h-[40px] ${className}`}
+  />
 );
 
 export default Avatar;
