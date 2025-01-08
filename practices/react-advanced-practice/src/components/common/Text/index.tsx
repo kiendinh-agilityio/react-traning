0f94064f17c010e0bc3c7e82777bdf420b9a28d1
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-08 15:12:09 +0700

    feat: update component Text

--- practices/react-advanced-practice/src/components/common/Text/index.tsx
+++ practices/react-advanced-practice/src/components/common/Text/index.tsx
@@ -1,5 +1,3 @@
-import { memo } from 'react';
-
 // Import radix ui
 import { Text as TextBase, TextProps } from '@radix-ui/themes';
 
@@ -11,20 +9,16 @@ type TextBaseProps = {
   size?: TextSize;
 } & TextProps;
 
-const Text = memo(
-  ({
-    content,
-    size = TextSize.Small,
-    as = 'p',
-    weight = 'regular',
-    className = '',
-  }: TextBaseProps) => {
-    return (
-      <TextBase as={as} weight={weight} className={`${size} font-regular ${className}`}>
-        {content}
-      </TextBase>
-    );
-  },
+const Text = ({
+  content,
+  size = TextSize.Small,
+  as = 'p',
+  weight = 'regular',
+  className = '',
+}: TextBaseProps) => (
+  <TextBase as={as} weight={weight} className={`${size} font-regular ${className}`}>
+    {content}
+  </TextBase>
 );
 
 export default Text;
