be5f633c2ab9b05cc8989b12969cfdacd010d8ce
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-08 14:18:35 +0700

    feat: implement storybook for component Text

---
+++ practices/react-advanced-practice/src/components/common/Text/Text.stories.tsx
@@ -0,0 +1,17 @@
+import Text from '.';
+
+export default {
+  component: Text,
+  title: 'Text',
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const TextContent = {
+  args: {
+    content:
+      'Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality).',
+  },
+};
