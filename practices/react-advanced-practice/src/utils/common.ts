6fc7dce768a0387d6e529fa5c49999227d5816a2
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-13 16:19:04 +0700

    feat: implement function currentYear

--- practices/react-advanced-practice/src/utils/common.ts
+++ practices/react-advanced-practice/src/utils/common.ts
@@ -4,3 +4,6 @@ import dayjs from 'dayjs';
 // Function format date
 export const formatDate = (date: string, dateFormat: string): string =>
   dayjs(date).format(dateFormat);
+
+// Get the current year
+export const currentYear = new Date().getFullYear();
