1dea311af9bf99e8fd032751da95314796dce3e8
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-14 16:45:56 +0700

    feat: create constants for messages

---
+++ practices/react-advanced-practice/src/constants/messages.ts
@@ -0,0 +1,6 @@
+export const MESSAGE_ERROR = {
+  REQUIRED_ERROR: (field: string) => `${field} is required`,
+  INVALID_EMAIL: 'Email is invalid',
+  INVALID_PASSWORD:
+    'Password must have at least 8 characters, 1 special character and 1 number. Example@123',
+};
