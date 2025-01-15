6fd15748342c7f627b0b9c0ec7d801b99939b847
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-15 16:14:02 +0700

    feat: implement constants messages

--- practices/react-advanced-practice/src/constants/messages.ts
+++ practices/react-advanced-practice/src/constants/messages.ts
@@ -3,4 +3,5 @@ export const MESSAGE_ERROR = {
   INVALID_EMAIL: 'Email is invalid',
   INVALID_PASSWORD:
     'Password must have at least 8 characters, 1 special character and 1 number. Example@123',
+  INVALID_NAME: 'Name must start with a capital letter, have no special characters',
 };
