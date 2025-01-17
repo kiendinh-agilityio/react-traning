2e286f3d07cf317d6e610b695a01af32bd686a72
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-17 17:44:54 +0700

    feat: update components SidebarItem

--- practices/react-advanced-practice/src/components/Sidebar/SidebarItem/index.tsx
+++ practices/react-advanced-practice/src/components/Sidebar/SidebarItem/index.tsx
@@ -7,14 +7,15 @@ interface SidebarItemProps {
   icon: React.ReactNode;
   label: string;
   active: boolean;
+  href: string;
 }
 
-const SidebarItem = memo(({ icon, label, active }: SidebarItemProps) => {
+const SidebarItem = memo(({ icon, label, active, href }: SidebarItemProps) => {
   const className = `sidebar-item ${active ? 'bg-white text-dark w-[220px] h-[54px] flex items-center rounded-[15px] cursor-pointer' : ''}`;
 
   return (
     <li className={className}>
-      <a className="flex items-center gap-4" href="#">
+      <a className="flex items-center gap-4" href={href}>
         <IconWrapper icon={icon} active={active} />
         <span className="text-xs leading-base">{label}</span>
       </a>
