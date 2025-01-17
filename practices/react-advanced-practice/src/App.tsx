0511c6f3a998bc030f15e05accfe90da44a0a5ad
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-17 17:47:03 +0700

    feat: update for pages

--- practices/react-advanced-practice/src/App.tsx
+++ practices/react-advanced-practice/src/App.tsx
@@ -1,5 +1,18 @@
-import { Home } from "@/pages";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-const App = () => <Home />;
+import { Home, Signin, Signup } from '@/pages';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
