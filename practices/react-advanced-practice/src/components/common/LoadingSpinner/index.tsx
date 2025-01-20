f3ebf852f49cbfed1fda320c43f9df74fd6e1bb1
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-20 17:25:04 +0700

    feat: implement ui for component Loading spinner

---
+++ practices/react-advanced-practice/src/components/common/LoadingSpinner/index.tsx
@@ -0,0 +1,10 @@
+// Import styles animation for toast
+import './loading.css';
+
+const LoadingSpinner = () => (
+  <div className="loading-container">
+    <span className="loading-indicator"></span>
+  </div>
+);
+
+export default LoadingSpinner;
