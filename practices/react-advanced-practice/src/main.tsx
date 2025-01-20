37a124169e8021484cc24efb701d4253181e0d66
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-20 17:19:23 +0700

    feat: update file main.tsx

--- practices/react-advanced-practice/src/main.tsx
+++ practices/react-advanced-practice/src/main.tsx
@@ -7,8 +7,14 @@ import App from './App.tsx';
 import '@radix-ui/themes/styles.css';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
