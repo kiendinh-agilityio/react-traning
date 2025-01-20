3b27e5eac26e8794d27e6b3eeb5a05cac10eb1b3
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-20 17:27:30 +0700

    feat: update constants BASE API

--- practices/react-advanced-practice/src/constants/api.ts
+++ practices/react-advanced-practice/src/constants/api.ts
@@ -1,1 +1,5 @@
+// Create api from json server auth
 export const API_AUTH_URL = 'http://localhost:3000';
+
+// Create api from mockapi
+export const BASE_API: string = process.env.VITE_API_URL || '';
