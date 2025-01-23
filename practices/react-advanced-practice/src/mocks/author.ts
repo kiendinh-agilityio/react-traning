343f872ef1a1bb0e169ba6136ee2dede10002743
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-23 15:56:04 +0700

    feat: create folder mock

---
+++ practices/react-advanced-practice/src/mocks/author.ts
@@ -0,0 +1,12 @@
+import { Author } from '@/types';
+
+export const PROFILE_AUTHORS: Author = {
+  id: '1',
+  name: 'Ken',
+  email: 'example@gmail.com',
+  avatarUrl: 'https://phimtat.vn/wp-content/uploads/2023/11/avt-3d.jpg',
+  position: 'Project',
+  roles: 'Designer',
+  status: 'Active',
+  date: '26/10/24',
+};
