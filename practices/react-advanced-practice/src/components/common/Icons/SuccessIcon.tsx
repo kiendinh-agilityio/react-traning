78ac9c3313dec933874e3bb3452c3627af848c94
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-23 14:15:09 +0700

    feat: implement common icons for pages

---
+++ practices/react-advanced-practice/src/components/common/Icons/SuccessIcon.tsx
@@ -0,0 +1,17 @@
+import { SVGProps } from 'react';
+
+const SuccessIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24px"
+    height="24px"
+    viewBox="0 0 512 512"
+    fill="#4fd1c5"
+    aria-label="Success Icon"
+    {...props}
+  >
+    <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+  </svg>
+);
+
+export default SuccessIcon;
