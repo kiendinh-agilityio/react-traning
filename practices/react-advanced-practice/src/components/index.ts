76de9489731a70a225ef5614f08502682f695683
Kien Dinh <kien.dinh@asnet.com.vn>
2025-01-23 16:59:03 +0700

    feat: update import AuthorForm

--- practices/react-advanced-practice/src/components/index.ts
+++ practices/react-advanced-practice/src/components/index.ts
@@ -6,3 +6,4 @@ export { default as SigninForm } from './SigninForm';
 export { default as SignupForm } from './SignupForm';
 export { default as SocialRegister } from './SocialRegister';
 export { default as InputGroup } from './InputGroup';
+export { default as AuthorForm } from './AuthorForm';
