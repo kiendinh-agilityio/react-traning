a300aef0fe5dd188a0a456abb9cadee7cec92b5f
Kien Dinh <kien.dinh@asnet.com.vn>
2025-02-07 17:24:56 +0700

    feat: implement logout for home page

--- practices/react-advanced-practice/src/pages/Home/Header/index.tsx
+++ practices/react-advanced-practice/src/pages/Home/Header/index.tsx
@@ -1,32 +1,53 @@
+import { useNavigate } from 'react-router-dom';
+
 // Import radix ui
 import { Flex, Box } from '@radix-ui/themes';
 
 // Import components common icon
-import { NotificationIcon, PinionIcon, UserIcon } from '@/components/common/Icons';
+import {
+  NotificationIcon,
+  PinionIcon,
+  UserIcon,
+  LogoutIcon,
+} from '@/components/common/Icons';
 
 // Import components
-import { Breadcrumb, Text } from '@/components/common';
+import { Breadcrumb, Text, Button } from '@/components/common';
 
 // Import components switch theme
 import { SwitchTheme } from '@/components';
+
+// Import types
+import { ButtonVariant } from '@/types';
 
 interface HeaderProps {
   currentPage: string;
 }
 
-const Header = ({ currentPage }: HeaderProps) => (
-  <header className="flex justify-between mb-7 px-[21px]">
-    <Box>
-      <Breadcrumb label="Pages" currentPage="Tables" />
-      <Text className="font-bold text-dark mt-[6px] dark:text-light">{currentPage}</Text>
-    </Box>
-    <Flex align="center" className="gap-[17px]">
-      <SwitchTheme />
-      <UserIcon color="#718096" className="cursor-pointer" />
-      <PinionIcon className="cursor-pointer" />
-      <NotificationIcon className="cursor-pointer" />
-    </Flex>
-  </header>
-);
+const Header = ({ currentPage }: HeaderProps) => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => navigate('/signin');
+
+  return (
+    <header className="flex justify-between mb-7 px-[21px]">
+      <Box>
+        <Breadcrumb label="Pages" currentPage="Tables" />
+        <Text className="font-bold text-dark mt-[6px] dark:text-light">
+          {currentPage}
+        </Text>
+      </Box>
+      <Flex align="center" className="gap-[17px]">
+        <SwitchTheme />
+        <UserIcon color="#718096" className="cursor-pointer" />
+        <PinionIcon className="cursor-pointer" />
+        <NotificationIcon className="cursor-pointer" />
+        <Button variant={ButtonVariant.Transparent} onClick={handleLogout}>
+          <LogoutIcon />
+        </Button>
+      </Flex>
+    </header>
+  );
+};
 
 export default Header;
