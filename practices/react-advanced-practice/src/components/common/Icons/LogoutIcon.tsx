3bb3a55092f9b7b63ff74395b5185363f8a1ca6f
Kien Dinh <kien.dinh@asnet.com.vn>
2025-02-07 17:24:36 +0700

    feat: add icon logout for page

---
+++ practices/react-advanced-practice/src/components/common/Icons/LogoutIcon.tsx
@@ -0,0 +1,59 @@
+import { SVGProps } from 'react';
+
+const LogoutIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    width={28}
+    height={28}
+    viewBox="0 0 256 256"
+    {...props}
+  >
+    <g
+      style={{
+        stroke: 'none',
+        strokeWidth: 0,
+        strokeDasharray: 'none',
+        strokeLinecap: 'butt',
+        strokeLinejoin: 'miter',
+        strokeMiterlimit: 10,
+        fill: 'none',
+        fillRule: 'nonzero',
+        opacity: 1,
+      }}
+    >
+      <path
+        d="M69.313 54.442a2 2 0 0 1-1.634-3.149l10.118-14.399-10.118-14.399a2 2 0 0 1 3.273-2.3l10.926 15.549c.484.69.484 1.61 0 2.3L70.952 53.592a1.998 1.998 0 0 1-1.639.85zM57.693 30.092a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H9.759c-.013 0-.024.003-.037.004-.037 0-.074.008-.111.011-.122.009-.24.027-.354.057-.046.011-.089.026-.133.041A1.952 1.952 0 0 0 8.8.255c-.025.014-.053.019-.077.034-.012.007-.02.018-.031.025-.13.083-.252.177-.36.287-.019.019-.033.042-.051.061a2.088 2.088 0 0 0-.294.429 2.032 2.032 0 0 0-.129.316c-.013.042-.028.082-.038.125A1.955 1.955 0 0 0 7.759 2v69.787c0 .17.028.333.068.49.011.043.025.083.039.124.04.123.091.239.152.35.019.033.034.068.054.1.086.135.185.26.3.371.022.021.047.037.07.058.102.09.214.169.333.237.021.012.037.03.058.042l31.016 16.213a2.004 2.004 0 0 0 1.962-.061A2.002 2.002 0 0 0 42.775 88V73.787h14.918a2 2 0 0 0 2-2V45a2 2 0 0 0-4 0v24.787H42.775V18.213c0-.745-.414-1.428-1.074-1.772L17.902 4h37.791v24.092a2 2 0 0 0 2 2z"
+        style={{
+          stroke: 'none',
+          strokeWidth: 1,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 10,
+          fill: '#718096',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        transform="matrix(2.81 0 0 2.81 1.407 1.407)"
+      />
+      <path
+        d="M80.241 38.894H47.536a2 2 0 0 1 0-4h32.705a2 2 0 0 1 0 4z"
+        style={{
+          stroke: 'none',
+          strokeWidth: 1,
+          strokeDasharray: 'none',
+          strokeLinecap: 'butt',
+          strokeLinejoin: 'miter',
+          strokeMiterlimit: 10,
+          fill: '#718096',
+          fillRule: 'nonzero',
+          opacity: 1,
+        }}
+        transform="matrix(2.81 0 0 2.81 1.407 1.407)"
+      />
+    </g>
+  </svg>
+);
+
+export default LogoutIcon;
