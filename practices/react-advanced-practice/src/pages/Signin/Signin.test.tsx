55a9b27a05c537004e41ac70d03edf1008bc9d77
Kien Dinh <kien.dinh@asnet.com.vn>
2025-02-12 17:35:15 +0700

    feat: write unit test for Signin page

---
+++ practices/react-advanced-practice/src/pages/Signin/Signin.test.tsx
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MemoryRouter } from 'react-router-dom';
+import Signin from '.';
+
+// Create a test QueryClient
+const createTestQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: { retry: false },
+    },
+  });
+
+describe('Signin Component', () => {
+  it('renders without crashing', () => {
+    const queryClient = createTestQueryClient();
+    const { container, asFragment } = render(
+      <MemoryRouter>
+        <QueryClientProvider client={queryClient}>
+          <Signin />
+        </QueryClientProvider>
+      </MemoryRouter>,
+    );
+
+    expect(container).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
