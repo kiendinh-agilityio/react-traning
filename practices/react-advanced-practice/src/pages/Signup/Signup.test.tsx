ae1dc3647d4a317390bf93a4bc17581c94526a1b
Kien Dinh <kien.dinh@asnet.com.vn>
2025-02-18 10:40:07 +0700

    feat: update unit test for Signup page

--- practices/react-advanced-practice/src/pages/Signup/Signup.test.tsx
+++ practices/react-advanced-practice/src/pages/Signup/Signup.test.tsx
@@ -1,7 +1,13 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MemoryRouter } from 'react-router-dom';
 import Signup from '.';
+import { registerUser } from '@/services';
+
+// Mock the registerUser service
+jest.mock('@/services', () => ({
+  registerUser: jest.fn(),
+}));
 
 // Create a test QueryClient
 const createTestQueryClient = () =>
@@ -11,7 +17,7 @@ const createTestQueryClient = () =>
     },
   });
 
-describe('Signin Component', () => {
+describe('Signup Component', () => {
   it('renders without crashing', () => {
     const queryClient = createTestQueryClient();
     const { container, asFragment } = render(
@@ -23,6 +29,59 @@ describe('Signin Component', () => {
     );
 
     expect(container).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('calls handleDownload when the download button is clicked', () => {
+    const queryClient = createTestQueryClient();
+    const { asFragment } = render(
+      <MemoryRouter>
+        <QueryClientProvider client={queryClient}>
+          <Signup />
+        </QueryClientProvider>
+      </MemoryRouter>,
+    );
+
+    const downloadButton = screen.getByText('Free Download');
+    fireEvent.click(downloadButton);
+
+    // Snapshot after clicking the button
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('calls handleSignupSubmit with the correct data when the form is submitted', async () => {
+    const queryClient = createTestQueryClient();
+    const { asFragment } = render(
+      <MemoryRouter>
+        <QueryClientProvider client={queryClient}>
+          <Signup />
+        </QueryClientProvider>
+      </MemoryRouter>,
+    );
+
+    // Mock the registerUser function to resolve successfully
+    (registerUser as jest.Mock).mockResolvedValueOnce({});
+
+    fireEvent.input(screen.getByPlaceholderText('Your full name'), {
+      target: { value: 'Test User' },
+    });
+    fireEvent.input(screen.getByPlaceholderText('Your email address'), {
+      target: { value: 'test@example.com' },
+    });
+    fireEvent.input(screen.getByPlaceholderText('Your password'), {
+      target: { value: 'Password@123' },
+    });
+    fireEvent.click(screen.getByText('Sign Up'));
+
+    // Wait for the registerUser function to be called
+    await waitFor(() => {
+      expect(registerUser).toHaveBeenCalledWith({
+        name: 'Test User',
+        email: 'test@example.com',
+        password: 'Password@123',
+      });
+    });
+
     expect(asFragment()).toMatchSnapshot();
   });
 });
