606c06bbe763b404321177790c21db1730ea019d
Kien Dinh <kien.dinh@asnet.com.vn>
2025-03-04 17:54:49 +0700

    feat: update component Toast

--- practices/react-advanced-practice/src/components/common/Toast/index.tsx
+++ practices/react-advanced-practice/src/components/common/Toast/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 // Import radix ui
 import { Box, Flex } from '@radix-ui/themes';
@@ -19,30 +19,19 @@ interface ToastProps {
   type: string;
   message: string;
   isOpen: boolean;
-  onClose?: () => void;
+  onClose: () => void;
 }
 
-const Toast = ({ type, message, isOpen: initialIsOpen, onClose }: ToastProps) => {
-  const [isOpen, setIsOpen] = useState<boolean>(initialIsOpen);
+const Toast = ({ type, message, isOpen, onClose }: ToastProps) => {
   const countTime = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-    // Update isOpen state when props initialIsOpen changes
-    setIsOpen(initialIsOpen);
-  }, [initialIsOpen]);
-
-  useEffect(() => {
-    // If isOpen is true, set timeout to automatically close Toast after 5 seconds
     if (isOpen) {
       countTime.current = setTimeout(() => {
-        setIsOpen(false);
-
-        // Call callback when Toast closes
         onClose && onClose();
       }, 3000);
     }
 
-    // Clean up timeouts when component unmounts or when isOpen changes
     return () => {
       if (countTime.current) {
         clearTimeout(countTime.current);
@@ -50,37 +39,23 @@ const Toast = ({ type, message, isOpen: initialIsOpen, onClose }: ToastProps) =>
     };
   }, [isOpen, onClose]);
 
-  const handleCloseToast = () => {
-    // Cancel the timeout and close the Toast as soon as the user clicks the close button
-    countTime.current && clearTimeout(countTime.current);
-
-    setIsOpen(false);
-
-    // Call callback when user closes Toast
-    onClose && onClose();
-  };
-
-  return (
-    <>
-      {isOpen && (
-        <Flex
-          align="center"
-          justify="between"
-          className={`toast ${type} w-[400px] p-4 bg-white rounded shadow-md m-auto absolute left-0 right-0 bottom-8`}
-        >
-          <Flex align="center" className="gap-4">
-            {type === 'success' ? <SuccessIcon /> : <FailedIcon />}
-            <Box as="span" className="font-regular text-base">
-              {message}
-            </Box>
-          </Flex>
-          <Button variant={ButtonVariant.Transparent} onClick={handleCloseToast}>
-            <CloseIcon />
-          </Button>
-        </Flex>
-      )}
-    </>
-  );
+  return isOpen ? (
+    <Flex
+      align="center"
+      justify="between"
+      className={`toast ${type} w-[400px] p-4 bg-white rounded shadow-md m-auto absolute left-0 right-0 bottom-8`}
+    >
+      <Flex align="center" className="gap-4">
+        {type === 'success' ? <SuccessIcon /> : <FailedIcon />}
+        <Box as="span" className="font-regular text-base">
+          {message}
+        </Box>
+      </Flex>
+      <Button variant={ButtonVariant.Transparent} onClick={onClose}>
+        <CloseIcon />
+      </Button>
+    </Flex>
+  ) : null;
 };
 
 export default Toast;
