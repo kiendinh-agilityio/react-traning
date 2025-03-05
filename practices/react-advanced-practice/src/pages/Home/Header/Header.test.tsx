ddcf628ede0a1864d997db67de67be4883045e75
Kien Dinh <kien.dinh@asnet.com.vn>
2025-03-05 15:15:55 +0700

    feat: update unit test for Header

--- practices/react-advanced-practice/src/pages/Home/Header/Header.test.tsx
+++ practices/react-advanced-practice/src/pages/Home/Header/Header.test.tsx
@@ -1,14 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Header from '.';
 
-describe('Header Component', () => {
-  const mockNavigate = jest.fn();
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+}));
 
-  jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: () => mockNavigate,
-  }));
+describe('Header Component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
   it('renders correctly and matches snapshot', () => {
     const { asFragment } = render(
@@ -26,6 +29,21 @@ describe('Header Component', () => {
       </MemoryRouter>,
     );
     expect(getByText('Dashboard')).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('calls navigate with /signin when logout button is clicked', () => {
+    const { getByLabelText, asFragment } = render(
+      <MemoryRouter>
+        <Header currentPage="Dashboard" />
+      </MemoryRouter>,
+    );
+
+    const logoutButton = getByLabelText('Button logout');
+    fireEvent.click(logoutButton);
+
+    expect(mockNavigate).toHaveBeenCalledTimes(1);
+    expect(mockNavigate).toHaveBeenCalledWith('/signin');
     expect(asFragment()).toMatchSnapshot();
   });
 });
